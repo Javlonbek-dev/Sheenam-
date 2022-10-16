850d53b96ef12ba448dca1b37e85d585aa2de02e
Azizbek Ochilov <99602759+azizbek-ochilov@users.noreply.github.com>
2022-10-16 13:09:24 +0500

    INFRA: Initialize Api Project

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿//==============================================
+//Copyright (c) Coalition Good-Hearted Engineers
+//Free to Use Comfort And Peace
+//==============================================
+
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api /[controller]")]
+    public class HomeController : RestfulController
+    {
+        [HttpGet]
+
+        public ActionResult<string> Get() => "Hello Mario, the princes is another castle.";
+
+
+    }
+}
